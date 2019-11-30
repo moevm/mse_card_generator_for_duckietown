9a98094b0cedb07dedc7d239974e4efecf76b8b2
IMConstant <kostjakov@gmail.com>
2019-12-01 01:10:35 +0300

    params fix

--- run.sh
+++ run.sh
@@ -10,6 +10,16 @@ case $key in
     shift
     shift
     ;;
+    -c|--crossroad_count)
+    CROSSROAD_COUNT="$2"
+    shift
+    shift
+    ;;
+    -l|--length)
+    LENGTH="$2"
+    shift
+    shift
+    ;;
     *)
     echo "unknown option ${1}"
     shift
@@ -21,6 +31,14 @@ if [[ -z ${MAP_SIZE} ]]; then
     MAP_SIZE="9x9"
 fi
 
+if [[ -z ${CROSSROAD_COUNT} ]]; then
+    CROSSROAD_COUNT="6"
+fi
 
-python3.7 main.py --size $MAP_SIZE
+if [[ -z ${LENGTH} ]]; then
+    LENGTH="10"
+fi
+
+
+python3.7 main.py --size $MAP_SIZE --crossroad_count $CROSSROAD_COUNT --road_length $LENGTH
 python3.7 ${DUCKIE_TOWN_PATH}/manual_control.py --env-name Duckietown-small_loop-v0 --seed 2 --map-name ./maps/new_map.yaml
