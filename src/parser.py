250f821b334058680f68a0c7d1cbec2e576bb3c5
IMConstant <kostjakov@gmail.com>
2019-12-03 02:03:19 +0300

    fix

--- src/parser.py
+++ src/parser.py
@@ -10,13 +10,13 @@ class Parser(object):
         parser = argparse.ArgumentParser()
         parser.add_argument('--size', default='7x7', type=str, action='store', help='map size')
         parser.add_argument('--crossroad_count', default='3', type=str, action='store', help='crossroads count and type\nFor example:\n4\n4T\n4Q\n4T2Q')
-        parser.add_argument('--road_length', default=10, type=str, action='store', help='maximum road length')
+        parser.add_argument('--road_length', default=15, type=str, action='store', help='maximum road length')
 
         width, height = list(map(int, (parser.parse_args().size).split('x')))
-        crossroads_count = int(parser.parse_args().crossroad_count)
-        triple = random.randint(0, crossroads_count // 2) * 2
-        quad = crossroads_count - triple
         length = int(parser.parse_args().road_length)
+        triple, quad = Parser.parse_crossroads(parser.parse_args().crossroad_count)
+
+        print(triple)
 
         return {
             'width': width,
@@ -27,3 +27,24 @@ class Parser(object):
                 'quad': quad
             }
         }
+
+    @classmethod
+    def parse_crossroads(cls, s: str):
+        if s.isdigit():
+            n = int(s)
+            t = random.randint(0, n // 2) * 2
+            q = n - t
+            return t, q
+        else:
+            data = s.split('.')
+            output = [0, 0]
+
+            for c in data:
+                it = 0 if c[-1] == 'T' else 1
+
+                if c[:-1].isdigit():
+                    output[it] = int(c[:-1])
+                elif c[:-1] == 'x':
+                    output[it] = random.randint(0, 3)
+
+            return output
