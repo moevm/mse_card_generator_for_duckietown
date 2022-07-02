ead690126b47adf071431e6d951f8629cbee1fa1
nixelpixel <dmitry.boblakov@yandex.ru>
2022-07-02 12:29:37 +0300

    it works 2

--- emptyMapGenerator/emptyMap.py
+++ emptyMapGenerator/emptyMap.py
@@ -4,18 +4,19 @@ import dt_maps
 
 def createMain():
     dict_file = {'main': {'frames': 'frames.yaml', 'tiles': 'tiles.yaml', 'tile_maps': 'tiles_maps.yaml'}}
+    return dict_file
 
-    # with open(r'./map/main.yaml', 'w') as file:
-    #     documents = yaml.dump(dict_file, file)
 
 def fb(a,b):
     return {f'map_0/tile_{a}_{b}': {'relative_to': 'map_0', 'pose': {'x': a, 'y': b,'z': 0, 'roll': 0, 'pitch': 0, 'yaw': 0 }}}
 
 def fl(size):
     my_dict = {}
+
     for i in range(0, size):
         for j in range(0, size):
             my_dict.update(fb(i, j))
+
 
     return my_dict
 
@@ -23,20 +24,18 @@ def fr():
      frames = fl(size)
      adding_part = {'map_0': {'relative_to': None, 'pose': {'x': 1.0, 'y': 2.0, 'z': 0.0, 'roll': 0, 'pitch': 0, 'yaw': 0}}}
      frames.update(adding_part)
-     result = {'frames': frames}
+     result = frames
      return result
 
 def createFrames():
     dict_file = fr()
     return dict_file
-    # with open(r'./map/frames.yaml', 'w') as file:
-    #     documents = yaml.dump(dict_file, file)
+
 
 def createTileMaps():
-    dict_file = {'tile_maps': {'map_0': {'tile_size': {'x': 0.585, 'y': 0.585}}}}
+    dict_file = {'map_0': {'tile_size': {'x': 0.585, 'y': 0.585}}}
+    return dict_file
 
-    # with open(r'./map/tile_maps.yaml', 'w') as file:
-    #     documents = yaml.dump(dict_file, file)
 
 def cr(a, b):
     return {f'map_0/tile_{a}_{b}': {'i': a, 'j': b, 'type': 'floor'}}
@@ -50,46 +49,32 @@ def cl(size):
     return my_dict
 
 def mp():
-    my_map = {'tiles': cl(size)}
+    my_map = cl(size)
     return my_map
 
 def createTiles():
     dict_file = mp()
     return dict_file
-    # with open(r'./map/tiles.yaml', 'w') as file:
-    #     documents = yaml.dump(dict_file, file)
-
-
-# def createEmptyMap():
-#     createMain()
-#    # createFrames()
-#     createTileMaps()
-    #createTiles()
 
 
 if __name__ == '__main__':
-    size = 5
-    # createEmptyMap()
-    # q_map = dt_maps.Map.from_disk("map_0", "./map")
-    # q_map._path = "./new_map"
-    # print(list(q_map.layers.items()))
-    # q_map.to_disk()
-
-    # M = dt_maps.Map("map_0", "./my_map")
+    size = 3
     frames_content = createFrames()
     tiles_content = createTiles()
-
-    # print(M.layers.has("frames"))
-    # M.to_disk()
+    tile_maps_content = createTileMaps()
 
     M = dt_maps.Map("map_0", "./my_map")
 
     frames_layer = dt_maps.MapLayer(M, "frames", frames_content)
+    frames_layer.write("map_0", ['pose', 'pitch'], 1)
     tiles_layer = dt_maps.MapLayer(M, "tiles", tiles_content)
-    # M.layers.frames = frames_content
+    tile_maps_layer = dt_maps.MapLayer(M, "tile_maps", tile_maps_content)
 
     # populate map
-    M._layers.__dict__["frames"] = frames_layer
+    M.layers.__dict__["frames"] = frames_layer
+    M.layers.__dict__["tiles"] = tiles_layer
+    M.layers.__dict__["tile_maps"] = tile_maps_layer
+    # M.layers.frames["map_0/tile_0_0"].pose.pitch = 1
 
     print(list(M.layers.items()))
     M.to_disk()
