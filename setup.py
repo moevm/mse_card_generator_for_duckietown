cca5660c52473a89988c22ef61aa776e21f59268
nixelpixel <dmitry.boblakov@yandex.ru>
2022-07-27 21:01:34 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dt-map-generator',
+    version='1.1.0',
+    packages=['src', 'src.basics'],
+    url='https://github.com/moevm/mse_map_generator_for_duckietown',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
