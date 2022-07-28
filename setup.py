7a07f1919ec1ff4e9885d093e1bf06bb9f275873
nixelpixel <dmitry.boblakov@yandex.ru>
2022-07-28 18:52:57 +0300

    added requirements

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.1.0',
     packages=['dt_map_generator', 'dt_map_generator.basics'],
     url='https://github.com/moevm/mse_map_generator_for_duckietown',
+    install_requires=['dt_maps'],
     license='',
     author='',
     author_email='',
