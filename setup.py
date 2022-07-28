4ef7fcc188c5c2643bb4849d92b5a7f502822c8e
nixelpixel <dmitry.boblakov@yandex.ru>
2022-07-28 19:03:56 +0300

    added requirements to py_modules

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.1.0',
     packages=['dt_map_generator', 'dt_map_generator.basics'],
     url='https://github.com/moevm/mse_map_generator_for_duckietown',
+    py_modules=["dt_maps"],
     install_requires=['dt_maps'],
     license='',
     author='',
